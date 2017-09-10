cb7d1fcc89bbca3c5f832c4245c7e680ab43a4d1
Ram Krishna Das <ramkdas12@gmail.com>
2017-09-10 15:38:20 +0530

    Adding Initial Code

---
+++ todoList/src/app/section/section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-section',
+    templateUrl: './section.component.html',
+    styleUrls: ['./section.component.css']
+})
+export class SectionComponent {
+}
