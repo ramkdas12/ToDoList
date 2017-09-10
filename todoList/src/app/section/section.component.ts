d53eb47237a57482402137f3a6b61444db1352a3
Ram Krishna Das <ramkdas12@gmail.com>
2017-09-10 21:44:06 +0530

    Created List Class, Updated CSS

--- todoList/src/app/section/section.component.ts
+++ todoList/src/app/section/section.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ListService } from './../list.service';
+import { List } from './../list.class';
+
 @Component({
     selector: 'app-section',
     templateUrl: './section.component.html',
     styleUrls: ['./section.component.css']
 })
 export class SectionComponent {
+    list: List[];
+    
 }
